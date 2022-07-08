46f0d3ecd6636a27d971a836132ceed6d8ff70b3
the4nna <the4nna@outlook.com>
2022-07-08 09:40:35 -0300

    Relação entre tipos e variáveis

--- src/aplicacao-js-ts/aplicacaoTS.js
+++ src/aplicacao-js-ts/aplicacaoTS.js
@@ -2,13 +2,20 @@
 var button = document.getElementById('button');
 var input1 = document.getElementById('input1');
 var input2 = document.getElementById('input2');
-function somarNumeros(num1, num2) {
-    return num1 + num2;
+function somarNumeros(num1, num2, printarResultado, frase) {
+    var resultado = num1 + num2;
+    if (printarResultado) {
+        console.log(frase + resultado);
+    }
+    return resultado;
 }
+var printarResultado = true;
+var frase;
+frase = "O resultado é: ";
 if (button) {
     button.addEventListener('click', function () {
         if (input1 && input2) {
-            console.log(somarNumeros(Number(input1.value), Number(input2.value)));
+            console.log(somarNumeros(Number(input1.value), Number(input2.value), printarResultado, frase));
         }
     });
 }
--- src/aplicacao-js-ts/aplicacaoTS.ts
+++ src/aplicacao-js-ts/aplicacaoTS.ts
@@ -2,15 +2,25 @@ let button = document.getElementById('button')
 let input1 = document.getElementById('input1') as HTMLInputElement
 let input2 = document.getElementById('input2') as HTMLInputElement
 
-function somarNumeros(num1: number, num2: number) {
-    return num1 + num2
+function somarNumeros(num1: number, num2: number, printarResultado: boolean, frase: string) {
+    const resultado = num1 + num2
+    if (printarResultado) {
+        console.log(frase + resultado)
+    }
+    return resultado;
 }
+
+let printarResultado = true;
+let frase: string;
+frase = "O resultado é: ";
+
 
 if (button) {
     button.addEventListener('click', () => {
         if (input1 && input2) {
-            console.log(somarNumeros(Number(input1.value), Number(input2.value)))
+            console.log(somarNumeros(Number(input1.value), Number(input2.value), printarResultado, frase))
         }
     })
 }
+
 
